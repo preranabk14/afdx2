a7736758971441f945decc71f6cfdf8e90994aaa
preranabk14 <preranabk04@gmail.com>
2024-08-09 12:10:15 +0530

    Update afdx2.cpp

--- afdx2.cpp
+++ afdx2.cpp
@@ -1,1 +1,16 @@
 #include <pcap.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init afdx_init(void) {
+    printk(KERN_INFO "AFDX Module Loaded.\n");
+    return 0;
+}
+
+static void __exit afdx_exit(void) {
+    printk(KERN_INFO "AFDX Module Unloaded.\n");
+}
+
+module_init(afdx_init);
+module_exit(afdx_exit);
+MODULE_LICENSE("GPL");
